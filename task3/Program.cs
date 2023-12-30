b9fd5cd8468e964518bf5ad21d3bcc16611207bf
Anatasia Andreeva <zemcova-anastasi@mail.ru>
2023-12-31 01:08:55 +0300

    Решила задачу 3

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.*/
+
+void FillPrintArray(int[] array, int index = 0)//метод для создания и печати массива
+{
+	if (index < array.Length)
+	{
+		
+		array[index] = new Random().Next(1, 10);
+		Console.Write(array[index] + " ");
+		FillPrintArray(array, index + 1);
+	}
+}
+
+void ReversArray(int[] array, int index)
+{
+	if (index >= 0)
+	{
+		Console.Write(array[index] + " ");
+		ReversArray(array, index - 1);
+	}	
+}
+int[] array = new int[5];
+FillPrintArray(array);
+Console.WriteLine();
+ReversArray(array, array.Length - 1);
